727062d5db2b5c815748f48d51d5e6a18893da57
nads205 <nads205@users.noreply.github.com>
2018-10-01 20:52:35 +0100

    adding file

---
+++ BankOcr.Tests/SampleGenerator.cs
@@ -0,0 +1,13 @@
+﻿using BankOcr.Code;
+
+namespace BankOcr.Tests
+{
+    class SampleGenerator
+    {
+        void GenerateLines()
+        {
+          //  var c = new Characters();
+        }
+
+    }
+}
